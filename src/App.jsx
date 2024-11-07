45ca413e19fa70ad26b21eede47172ed140b6c17
Paras Bagri <parasba85@gmail.com>
2024-11-07 15:42:39 +0530

    Router, Rout,

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Hearder/Header.component'
+import { BrowserRouter as Router,Route, Link } from "react-router-dom";
+import Footer from './components/Footer/Footer.component';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Router>
+       <Header/>
+     </Router>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Footer/Footer.component.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+   return (
+    <>
+    <h1>Hello Paras</h1>
+    </>
+   )
+  }
+
+  export default Footer;
