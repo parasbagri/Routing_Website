d132337d71691164ecf90b9ab67ca7599f84f431
Paras Bagri <parasba85@gmail.com>
2024-11-08 13:12:03 +0530

    used route Loader and their Hook {useLoaderData} fetched API

--- src/components/GitHub/Github.component.jsx
+++ src/components/GitHub/Github.component.jsx
@@ -1,21 +1,24 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
+// ye hai component jo main.jsx me Github ke naam se upload hoga 
 const Github = () => {
-
+    // direct values dega useLoaderData ();
+    const {followers,avatar_url} = useLoaderData();
     // const gitAPI = 'https://api.github.com/repos/octocat/Spoon-Knife/issues';
     // const [issues, setIssues] = useState('');
-    const [data, setData] = useState('');
-     // mai api call karna chataha hu kab jab ye componenet load ho uske baad to useEffect() karta hai 
-     useEffect(()=>{
-        fetch('https://api.github.com/users/parasbagri')
-        .then(res => res.json()).then((res)=> {
-            setData(res)
-            console.log(res)})
-        //.then(data => setIssues(data))
-        //.catch(error => console.log(error))
-    },[])
-    const {avatar_url, followers} = data;
+    // const [data, setData] = useState('');
+    //  // mai api call karna chataha hu kab jab ye componenet load ho uske baad to useEffect() karta hai 
+    //  useEffect(()=>{
+    //     fetch('https://api.github.com/users/parasbagri')
+    //     .then(res => res.json()).then((res)=> {
+    //         setData(res)
+    //         console.log(res)})
+    //     //.then(data => setIssues(data))
+    //     //.catch(error => console.log(error))
+    // },[])
+    // const {avatar_url, followers} = data;
     return(   
         <>
        <div>
@@ -29,3 +32,10 @@ const Github = () => {
 }
 
 export default Github;
+
+// ye hai method jo alag se main.jsx me import hoga {gitHubInfoLoader} kuch aise 
+export const gitHubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/parasbagri');
+    const data = await response.json();
+    return data;
+}
